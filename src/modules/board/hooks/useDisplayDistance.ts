6edefbe3a01482ff319f07933ba58416584ceb70
Jonah Möller <j.moeller@mytaxi.com>
2021-01-09 17:48:21 +0100

    fix: remove unused variable

--- src/modules/board/hooks/useDisplayDistance.ts
+++ src/modules/board/hooks/useDisplayDistance.ts
@@ -1,8 +1,4 @@
-import { useUserPreferences } from '../../preferences/hooks/useUserPreferences';
-
 export function useDisplayDistance() {
-    const preferences = useUserPreferences();
-
     return (rawDistance: number) => {
         return `${rawDistance * 5}&nbsp;ft\n${rawDistance}&nbsp;sq`
     }
