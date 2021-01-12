0030cbe87d10b9a432fa4b757f4554898110cebc
Jonah Möller <j.moeller@mytaxi.com>
2021-01-12 17:07:45 +0100

    chore: switch to https for socket connection

--- src/shared/connection.ts
+++ src/shared/connection.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-export const connection = io('http://twentyone-300010.appspot.com', {
+export const connection = io('https://twentyone-300010.appspot.com', {
     transports: [ 'websocket' ],
     reconnectionAttempts: 3
 });
