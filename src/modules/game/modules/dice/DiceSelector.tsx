11170fa3dee05d211a270243a2a9350225cbccc4
Jonah Möller <j.moeller@mytaxi.com>
2021-01-22 00:35:11 +0100

    feat: move dice roll button to top

--- src/modules/game/modules/dice/DiceSelector.tsx
+++ src/modules/game/modules/dice/DiceSelector.tsx
@@ -1,24 +1,23 @@
 import { useStyletron } from 'baseui';
 import { Button } from 'baseui/button';
 import React, { useState } from 'react';
+import { animated, useSpring } from 'react-spring';
 import { useGameStore } from '../../hooks/useGameStore';
 import { DiceType } from './hooks/useCreateDice';
-
-function SelectedDice(props: { amount: number }) {
-    if (props.amount > 0) {
-        return <>x{props.amount}</>;
-    }
-
-    return null;
-}
 
 export function DiceSelector() {
     const [ css, theme ] = useStyletron();
     const [ selectedDice, setSelectedDice ] = useState<DiceType[]>([]);
     const setDice = useGameStore(state => state.setDice);
 
+    const [ style, set ] = useSpring(() => ({
+        bottom: 2,
+        opacity: 0
+    }));
+
     const handleDiceAdd = (diceType: DiceType) => (e: React.MouseEvent) => {
         setSelectedDice(current => current.concat([ diceType ]));
+        set({ bottom: 6, opacity: 1 });
     };
 
     const handleRollClick = () => {
@@ -27,44 +26,60 @@ export function DiceSelector() {
             result: 1
         }));
 
-        console.log(rollDice);
-
         setSelectedDice([]);
         setDice(rollDice);
+        set({ bottom: 2, opacity: 0 });
     };
 
     return (
-        <div
-            className={css({
-                position: 'absolute',
-                bottom: '2em',
-                left: 'calc(50% - 160px)',
-                transform: 'translateX(-50%)',
-                textAlign: 'center',
-                boxShadow: theme.lighting.shadow600,
-                background: theme.colors.backgroundTertiary
-            })}
-        >
-            <Button kind="secondary" onClick={handleDiceAdd('d4')}>
-                D4 <SelectedDice amount={selectedDice.filter(it => it === 'd4').length} />
-            </Button>
-            <Button kind="secondary" onClick={handleDiceAdd('d6')}>
-                D6 <SelectedDice amount={selectedDice.filter(it => it === 'd6').length} />
-            </Button>
-            <Button kind="secondary" onClick={handleDiceAdd('d8')} disabled>
-                D8
-            </Button>
-            <Button kind="secondary" onClick={handleDiceAdd('d10')} disabled>
-                D10
-            </Button>
-            <Button kind="secondary" onClick={handleDiceAdd('d12')}>
-                D12 <SelectedDice amount={selectedDice.filter(it => it === 'd12').length} />
-            </Button>
-            <Button kind="secondary" onClick={handleDiceAdd('d20')}>
-                D20 <SelectedDice amount={selectedDice.filter(it => it === 'd20').length} />
-            </Button>
+        <>
+            <animated.div
+                className={css({
+                    position: 'absolute',
+                    left: 'calc(50% - 160px)',
+                    bottom: '6em',
+                    transform: 'translateX(-50%)',
+                    textAlign: 'center',
+                    boxShadow: theme.lighting.shadow600
+                })}
+                style={{
+                    opacity: style.opacity,
+                    bottom: style.bottom.interpolate(b => `${b}em`)
+                }}
+            >
+                <Button onClick={handleRollClick}> Roll!</Button>
+            </animated.div>
 
-            {selectedDice.length > 0 && <Button onClick={handleRollClick} $style={{marginLeft: theme.sizing.scale400}}>Roll!</Button>}
-        </div>
+            <div
+                className={css({
+                    position: 'absolute',
+                    bottom: '2em',
+                    left: 'calc(50% - 160px)',
+                    transform: 'translateX(-50%)',
+                    textAlign: 'center',
+                    boxShadow: theme.lighting.shadow600,
+                    background: theme.colors.backgroundTertiary
+                })}
+            >
+                <Button kind="secondary" onClick={handleDiceAdd('d4')}>
+                    D4
+                </Button>
+                <Button kind="secondary" onClick={handleDiceAdd('d6')}>
+                    D6
+                </Button>
+                <Button kind="secondary" onClick={handleDiceAdd('d8')} disabled>
+                    D8
+                </Button>
+                <Button kind="secondary" onClick={handleDiceAdd('d10')} disabled>
+                    D10
+                </Button>
+                <Button kind="secondary" onClick={handleDiceAdd('d12')}>
+                    D12
+                </Button>
+                <Button kind="secondary" onClick={handleDiceAdd('d20')}>
+                    D20
+                </Button>
+            </div>
+        </>
     );
 }
